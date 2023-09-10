0f68e90d1f142399675dc32c84af44e483f0737f
vanshsharma7 <thevanshsharma@gmail.com>
2023-09-11 01:27:49 +0530

    Project Files

--- Assets/Script/Quiz.cs
+++ Assets/Script/Quiz.cs
@@ -2,16 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questiontext;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
     void Start()
     {
         questiontext.text=question.GetQuestion();
-        TextMeshProUGUI buttonText=answerButtons[0].GetComponentInChildren<TextMeshProUGUI>();
-        buttonText.text=question.GetAnswer(0);
+        for(int i=0;i<answerButtons.Length;i++){
+            TextMeshProUGUI buttonText=answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text=question.GetAnswer(i);
+        }
     }
+    public void OnAnswerSelect(int index)
+    {
+        Image buttonImage;
+        if(index==question.GetCorrectAnswerIndex()){
+            questiontext.text="Correct";
+            buttonImage=answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite=correctAnswerSprite;
+        }
+        else{
+            correctAnswerIndex=question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questiontext.text = "Oops, the correct answer was :\n"+ correctAnswer;
+            buttonImage=answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite=correctAnswerSprite;
+        }
+    } 
 }
