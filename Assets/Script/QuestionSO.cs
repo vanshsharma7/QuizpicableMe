8474854052e22ec4e8fe3ac5268378675b0e0f45
vanshsharma7 <thevanshsharma@gmail.com>
2023-09-10 19:22:27 +0530

    Project Files

--- Assets/Script/QuestionSO.cs
+++ Assets/Script/QuestionSO.cs
@@ -8,8 +8,16 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,4)]
     [SerializeField] string question="Enter question here :^)";
+    [SerializeField] string[] answers=new string[4];
+    [SerializeField] int CorrectAnswerIndex;
     public string GetQuestion(){
         return question;
+    }
+    public int GetCorrectAnswerIndex(){
+        return CorrectAnswerIndex;
+    }
+    public string GetAnswer(int index){
+        return answers[index];
     }
 }
 
---
+++ Assets/Script/Quiz.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI questiontext;
+    [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
+    void Start()
+    {
+        questiontext.text=question.GetQuestion();
+        TextMeshProUGUI buttonText=answerButtons[0].GetComponentInChildren<TextMeshProUGUI>();
+        buttonText.text=question.GetAnswer(0);
+    }
+}
